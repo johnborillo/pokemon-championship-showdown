55b8d97cff2d8dbebfe0703b48ad97b4a7d56a1f
John Borillo <73507954+johnborillo@users.noreply.github.com>
2023-02-03 16:03:18 -0500

    Added more styling to game, also added score board

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 // MODEL
-let selection = ["charizard", "blastoise", "venusaur"];
+let selection = ["Charizard", "Blastoise", "Venusaur"];
 let compScore = 0;
 let playerScore = 0;
 let roundCount = 0;
@@ -16,6 +16,7 @@ function getPlayerChoice(choice) {
     if (roundCount < 5) {
         playRound(getComputerChoice(), choice);
         roundCount++;
+        printRound(roundCount);
     } else {
         console.log("The game is over. Final score: Computer score: " + compScore + " | Player score: " + playerScore);
     }
@@ -28,6 +29,8 @@ function getComputerChoice() {
 function playRound(compChoice, playerChoice) {
     let comp = compChoice.toLowerCase();
     let player = playerChoice;
+    printPlayerChoice(playerChoice);
+    printCompChoice(compChoice);
     if (comp === player) {
         console.log("Computer chose: " + comp);
         console.log("TIED!")
@@ -47,4 +50,37 @@ function playRound(compChoice, playerChoice) {
         playerScore++;
         console.log("Computer score: " + compScore + " | Player score: " + playerScore);
     }
+    printScore(compScore, playerScore);
+}
+
+function printRound(rounds) {
+    const currentRound = document.getElementById('currentRound');
+    const element = document.createElement('h2');
+    currentRound.innerHTML = '';
+    element.innerText = 'ROUND ' + rounds;
+    currentRound.appendChild(element);
+}
+
+function printPlayerChoice(pokemonChosen){
+    const playerChoiceDiv = document.getElementById('playerChoice');
+    playerChoiceDiv.innerHTML = '';
+    const element = document.createElement('h3');
+    element.innerText = "You chose " + pokemonChosen;
+    playerChoiceDiv.appendChild(element);
+}
+
+function printCompChoice(pokemonChosen) {
+    const compChoiceDiv = document.getElementById('compChoice');
+    const element = document.createElement('h3');
+    compChoiceDiv.innerHTML = '';
+    element.innerText = "Ash chose " + pokemonChosen;
+    compChoiceDiv.appendChild(element);
+}
+
+function printScore(compScore, playerScore){
+    const scoreDiv = document.getElementById('score');
+    scoreDiv.innerHTML = '';
+    const element = document.createElement('h3');
+    element.innerText = "You: " + playerScore + "  Ash: " + compScore;
+    scoreDiv.appendChild(element);
 }
