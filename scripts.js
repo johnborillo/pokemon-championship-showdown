8151fa1c45a8ed4ee4839b492b1ddd877609a24a
John Borillo <73507954+johnborillo@users.noreply.github.com>
2023-02-02 22:10:39 -0500

    Reworked the game so that buttons are used as opposed to prompts. Game now goes for only 5 rounds.

--- scripts.js
+++ scripts.js
@@ -1,37 +1,41 @@
-const rock = "Rock";
-const paper = "Paper";
-const scissors = "Scissors";
-
-let selection = [rock, paper, scissors];
+// MODEL
+let selection = ["rock", "paper", "scissors"];
 let compScore = 0;
 let playerScore = 0;
+let roundCount = 0;
 
-const sleep = async (milliseconds) => {
-    await new Promise(resolve => {
-        return setTimeout(resolve, milliseconds)
-    });
-};
+// VIEW
+const playerChoiceFunctions = [
+    function () { return "rock"; },
+    function () { return "paper"; },
+    function () { return "scissors"; }
+]
+
+
+// CONTROLLER
+function getPlayerChoice(choice) {
+    if (roundCount < 5) {
+        playRound(getComputerChoice(), choice);
+        roundCount++;
+    } else {
+        console.log("The game is over. Final score: Computer score: " + compScore + " | Player score: " + playerScore);
+    }
+}
 
 function getComputerChoice() {
-    return selection[Math.floor(Math.random() * selection.length)]
-    // console.log(selection[Math.floor(Math.random() * 3)])
+    return selection[Math.floor(Math.random() * selection.length)];
 }
 
-function getPlayerChoice(){
-    let choice = prompt("Rock, Paper, or Scissors?");
-    return choice;
-}
-
-function playRound(compChoice, playerChoice){
+function playRound(compChoice, playerChoice) {
     let comp = compChoice.toLowerCase();
-    let player = playerChoice.toLowerCase();
-    if (comp === player){
+    let player = playerChoice;
+    if (comp === player) {
         console.log("Computer chose: " + comp);
         console.log("TIED!")
         console.log("Computer score: " + compScore + " | Player score: " + playerScore);
-    } else if(
+    } else if (
         (comp === "rock" && player === "scissors") ||
-        (comp === "scirrors" && player === "paper") ||
+        (comp === "scissors" && player === "paper") ||
         (comp === "paper" && player === "rock")
     ) {
         console.log("Computer chose: " + comp);
@@ -45,12 +49,3 @@ function playRound(compChoice, playerChoice){
         console.log("Computer score: " + compScore + " | Player score: " + playerScore);
     }
 }
-
-async function game(){
-    for(let i = 0; i < 5; i++){
-        playRound(getComputerChoice(), getPlayerChoice());
-        await sleep(3000);
-    }
-}
-
-game();
